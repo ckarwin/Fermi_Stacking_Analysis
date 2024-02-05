e2d9215756ef61491b8e9a3d627bf78b8d2ca878
Christopher Karwin <ckarwin@login003.palmetto.clemson.edu>
2024-02-05 11:09:01 -0500

    all

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,10 @@ author = 'Chris Karwin'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 extensions = ['sphinx.ext.mathjax',
               'sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
