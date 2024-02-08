43b4983dad46bde943db43c116164914c4c34a13
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2024-02-08 14:52:31 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ extensions = ['sphinx.ext.mathjax',
 
 
 autodoc_mock_imports = ['fermipy','pyLikelihood','BinnedAnalysis','IntegralUpperLimit',
-                        'UpperLimits','SummedLikelihood','scipy','pyyaml','pandas','matplotlib','astropy']
+                        'UpperLimits','SummedLikelihood','scipy']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
